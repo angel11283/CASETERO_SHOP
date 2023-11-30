98a89554efa32d35f3605b50d4bc3e1f82f8dca5
angel11283 <132527432+angel11283@users.noreply.github.com>
2023-11-29 22:20:19 -0600

    Terminando la función de agregar productos al carrito

--- src/Componentes/ProductList.jsx
+++ src/Componentes/ProductList.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import { data } from '../Data'
 
 
-export const ProductList = ({ allProducts, setAllProducts }) => {
-  const onAddProduct = () => {
-    console.log("agregado");
+export const ProductList = ({allProducts,setAllProducts,countProducts,setCountProducts,total,setTotal,
+}) => {
+	const onAddProduct = product => {
+		if (allProducts.find(item => item.id === product.id)) {
+			const products = allProducts.map(item =>
+				item.id === product.id
+					? { ...item, quantity: item.quantity + 1 }
+					: item
+			);
+			setTotal(total + product.price * product.quantity);
+			setCountProducts(countProducts + product.quantity);
+			return setAllProducts([...products]);
+		}
+
+		setTotal(total + product.price * product.quantity);
+		setCountProducts(countProducts + product.quantity);
+		setAllProducts([...allProducts, product]);
   }
+  console.log(allProducts)
   return (
-    <div className=''>
+    <div className='container-items'>
       {data.map(product => (
         <div className='item' key={product.id}>
           <figure>
@@ -20,7 +35,7 @@ export const ProductList = ({ allProducts, setAllProducts }) => {
           <div className="info-product">
             <h2>{product.name_product}</h2>
             <p className="price">${product.price}</p>
-            <button onClick={() => onAddProduct()} className="button">Añadir al carrito</button>
+            <button onClick={() => onAddProduct(product)} className="button">Añadir al carrito</button>
           </div>
         </div>
       ))}
--- src/Data.js
+++ src/Data.js
@@ -8,7 +8,7 @@ export const data = [
     },
     {
         id: 2,
-        name_product: 'Combo Orange Combo CRUSH 35RT',
+        name_product: 'Combo Orange CRUSH 35RT',
         price: 8000,
         urlImage: 'https://musicvilla.com/cdn/shop/products/image_01294ff1-a7c8-4403-840c-e19623c1b580_1024x1024.jpg?v=1646447329',
         quantity: 1
@@ -25,6 +25,27 @@ export const data = [
         name_product: 'Batería Gretsch S/STANDS CAT. MPL PLATA SPARKLE',
         price: 49652,
         urlImage: 'https://www.falymusic.com/images/detailed/19/CM1-E826P-SS.png',
+        quantity: 1
+    },
+    {
+        id:5,
+        name_product: 'Tomshin Saxofón, Saxofón Soprano BB',
+        price: 3987,
+        urlImage: 'https://m.media-amazon.com/images/I/61IqsC5IR5L._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id:6,
+        name_product: 'FENDER PLAYER JAZZ BASS BLACK BAJO ELÉCTRICO',
+        price: 16000,
+        urlImage: 'https://eusica.mx/product/image/large/0149902506_player_jazz_bass_0149902506_1.jpg',
+        quantity: 1
+    },
+    {
+        id:7,
+        name_product: 'Fender Rumble Series 100 Transistor',
+        price: 8000,
+        urlImage: 'https://http2.mlstatic.com/D_NQ_NP_670606-MLA32659602863_102019-O.webp',
         quantity: 1
     }
 ]
